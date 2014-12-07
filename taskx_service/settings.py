a41516ab9b09b20009687227f1c5220e3995fd49
Mark Burnett <mark.m.burnett@gmail.com>
2014-12-07 15:38:30 -0600

    Revert "get BASE_DIR explicitly from config"
    This reverts commit 05d9e7c144d2e1225e9a56e0350f2131441a9d89.

--- taskx_service/settings.py
+++ taskx_service/settings.py
@@ -6,9 +6,6 @@ TASKX_APP_NAME = os.environ['TASKX_APP_NAME']
 TASKX_APP_MODULE = os.environ['TASKX_APP_MODULE']
 
 
-BASE_DIR = os.path.abspath(os.environ['BASE_DIR'])
-
-
 # Security settings
 SECRET_KEY = os.environ['SECRET_KEY']
 
