58737b545204527b01172bc3d401c591e7be80e1
Mark Burnett <mark.m.burnett@gmail.com>
2014-12-07 13:00:13 -0600

    update views import

--- taskx_service/urls.py
+++ taskx_service/urls.py
@@ -3,7 +3,7 @@ from django.conf.urls import url, include
 from rest_framework import routers
 import importlib
 
-views = importlib.import_module('views', settings.TASKX_APP_MODULE)
+views = importlib.import_module('%s.views' % settings.TASKX_APP_MODULE)
 
 router = routers.DefaultRouter()
 router.register(r'tasks', views.TaskViewSet)
