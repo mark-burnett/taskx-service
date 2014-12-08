e8e11da45a9e063df192859268a77539848e7f2c
Mark Burnett <mark.m.burnett@gmail.com>
2014-12-07 20:13:28 -0600

    rely on urlpatterns provided by the app

--- taskx_service/urls.py
+++ taskx_service/urls.py
@@ -1,19 +1,12 @@
 from django.conf import settings
 from django.conf.urls import url, include
-from rest_framework import routers
-import django.contrib.staticfiles.views
+from django.contrib.staticfiles import views as static_views
 import importlib
 
-views = importlib.import_module('%s.views' % settings.TASKX_APP_MODULE)
+APP_URLS = importlib.import_module('%s.urls' % settings.TASKX_APP_MODULE)
 
-router = routers.DefaultRouter()
-router.register(r'tasks', views.TaskViewSet)
-
-# Wire up our API using automatic URL routing.
-# Additionally, we include login URLs for the browseable API.
 urlpatterns = [
-    url(r'^', include(router.urls)),
     url(r'^api-auth/', include('rest_framework.urls',
                                namespace='rest_framework')),
-    url(r'^static/(?P<path>.*)$', django.contrib.staticfiles.views.serve),
-]
+    url(r'^static/(?P<path>.*)$', static_views.serve),
+] + APP_URLS.urlpatterns
